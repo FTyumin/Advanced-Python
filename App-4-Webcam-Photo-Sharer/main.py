6f95cf0d2ba91fc9aa2fc9c5ecf30fcd90f64035
FTyumin <feodor.tjumin28@gmail.com>
2022-08-19 12:07:52 +0300

    Improved camera function

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import  ScreenManager, Screen
 from kivy.lang import Builder
+import time
 from filesharer import FileSharer
 
 
@@ -9,13 +10,18 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
-
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start camera"
+        self.ids.camera.texture = None
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
 
 class ImageScreen(Screen):
     pass
