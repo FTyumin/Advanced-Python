6bb24ba56f8824fe2d0e3a1d0da9f7d3e3d1d36c
FTyumin <feodor.tjumin28@gmail.com>
2022-08-19 10:44:45 +0300

    Jauns app

--- App-4-Webcam-Photo-Sharer/main.py
+++ App-4-Webcam-Photo-Sharer/main.py
@@ -1,9 +1,24 @@
 from kivy.app import App
 from kivy.uix.screenmanager import  ScreenManager, Screen
+from kivy.lang import Builder
+from filesharer import FileSharer
 
-class FirstScreen(Screen):
-    def search_image(self):
+
+
+Builder.load_file('frontend.kv')
+
+class CameraScreen(Screen):
+    def start(self):
         pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+class ImageScreen(Screen):
+    pass
 
 class RootWidget(ScreenManager):
     pass
@@ -11,6 +26,6 @@ class RootWidget(ScreenManager):
 class MainApp(App):
 
     def build(self):
-        return RootWidget()
+        return  RootWidget()
 
-
+MainApp().run()
