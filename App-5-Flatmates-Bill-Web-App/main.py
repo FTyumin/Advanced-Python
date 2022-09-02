1717cae2df6f60fb38259c685ae207064be19185
FTyumin <feodor.tjumin28@gmail.com>
2022-09-02 17:36:20 +0300

    Deployment, minor changes

--- App-5-Flatmates-Bill-Web-App/main.py
+++ App-5-Flatmates-Bill-Web-App/main.py
@@ -19,6 +19,31 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
 
+    def post(self):
+        billform = BillForm(request.form)
+        amount = billform.amount.data
+        period = billform.period.data
+
+        name1 = billform.name1.data
+        days_in_house1 = billform.days_in_house1.data
+
+        name2 = billform.name2.data
+        days_in_house2 = billform.days_in_house2.data
+
+        the_bill = flat.Bill(float(amount), period)
+        flatmate1 = flat.Flatmate(name1, float(days_in_house1))
+        flatmate2 = flat.Flatmate(name2, float(days_in_house2))
+
+        # return f"{flatmate1.name} pays {round(flatmate1.pays(the_bill,flatmate2),2)}"
+        return render_template('bill_form_page.html',
+                               result=True,
+                               billform=billform,
+                               name1=flatmate1.name,
+                               amount1=round(flatmate1.pays(the_bill, flatmate2), 2),
+                               name2=flatmate2.name,
+                               amount2=round(flatmate2.pays(the_bill, flatmate2), 2))
+
+
 class ResultsPage(MethodView):
 
     def post(self):
@@ -59,9 +84,9 @@ class BillForm(Form):
 
 app.add_url_rule('/',
                  view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill_form',
+app.add_url_rule('/bill_form_page',
                  view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results',
-                 view_func=ResultsPage.as_view('results_page'))
+# app.add_url_rule('/results',
+#                  view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
