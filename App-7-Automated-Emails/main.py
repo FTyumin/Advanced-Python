4cc1b101260d0d425a0ee82f9ee1ed33c0f82caa
FTyumin <feodor.tjumin28@gmail.com>
2022-10-19 21:42:18 +0300

    OOP

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -5,24 +5,37 @@ from pprint import pprint
 
 class NewsFeed:
 
-    def __init__(self, data):
-        self.data = data
+
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "f450b62759394d49a40a076cd24f1406"
+
+
+    def __init__(self, interest, from_date, to_date, language):
+
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = f"{self.base_url}" \
+              f"q={self.interest}&" \
+              f"from={self.from_date}&"\
+              f"to={self.to_date}&"\
+              f"language={self.language}" \
+              f"&apiKey={self.api_key}"
 
-url = "https://newsapi.org/v2/everything?" \
-      "q=putin&" \
-      "from=2022-09-10&" \
-      "language=en" \
-      "&apiKey=f450b62759394d49a40a076cd24f1406"
-response = requests.get(url)
-content = response.json()
-articles = content['articles']
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
 
 
-email_body = ''
-for article in articles:
-    email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+news_feed = NewsFeed(interest='Biden', from_date='2022-10-18', to_date='2022-10-19', language='en')
+print(news_feed.get())
 
-print(email_body)
