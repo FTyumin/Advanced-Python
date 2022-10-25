4e0ef1cb68bb9b5eb7b0ac34365291a3a97eb1f6
FTyumin <feodor.tjumin28@gmail.com>
2022-10-25 22:29:29 +0300

    Automating emails

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,41 +1,15 @@
-# api key - f450b62759394d49a40a076cd24f1406
-import requests
-from pprint import pprint
+import yagmail
+import pandas
+from news import NewsFeed
 
+df = pandas.read_excel('people.xlsx')
 
-class NewsFeed:
+for index, row in df.iterrows():
+    news_feed = NewsFeed(interest=row['interest'], from_date='2022-24-10', to_date = '2022-25-10')
 
-
-    base_url = "https://newsapi.org/v2/everything?"
-    api_key = "f450b62759394d49a40a076cd24f1406"
-
-
-    def __init__(self, interest, from_date, to_date, language):
-
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
-
-    def get(self):
-        url = f"{self.base_url}" \
-              f"q={self.interest}&" \
-              f"from={self.from_date}&"\
-              f"to={self.to_date}&"\
-              f"language={self.language}" \
-              f"&apiKey={self.api_key}"
-
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
-
-        email_body = ''
-        for article in articles:
-            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
-
-        return email_body
-
-
-news_feed = NewsFeed(interest='Biden', from_date='2022-10-18', to_date='2022-10-19', language='en')
-print(news_feed.get())
+    email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
+    email.send(to=row['email'],
+           subject=f"Your {row['interest']} news for today",
+           contents=f"Hello row['name']!\n See what's on about {row['interest']} today.{news_feed.get()}\n Teodors",
+           attachments="design.txt")
 
---
+++ App-7-Automated-Emails/news.py
@@ -0,0 +1,42 @@
+# api key - f450b62759394d49a40a076cd24f1406
+import requests
+from pprint import pprint
+
+
+class NewsFeed:
+
+
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "f450b62759394d49a40a076cd24f1406"
+
+
+    def __init__(self, interest, from_date, to_date, language='en'):
+
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
+
+    def get(self):
+        url = f"{self.base_url}" \
+              f"q={self.interest}&" \
+              f"from={self.from_date}&"\
+              f"to={self.to_date}&"\
+              f"language={self.language}" \
+              f"&apiKey={self.api_key}"
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
+
+        return email_body
+
+if __name__ == "__main__":
+    news_feed = NewsFeed(interest='Biden', from_date='2022-10-18', to_date='2022-10-19', language='en')
+    print(news_feed.get())
+
+
