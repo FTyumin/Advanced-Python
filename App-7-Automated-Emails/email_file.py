84b04aafed7c4e42f8f02dd23007080183c62f83
FTyumin <feodor.tjumin28@gmail.com>
2022-10-25 21:34:26 +0300

    Sending emails

---
+++ App-7-Automated-Emails/email_file.py
@@ -0,0 +1,10 @@
+import yagmail
+import pandas
+
+
+
+email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
+email.send(to="zbrwkhskc@laste.ml",
+           subject="Hi friend",
+           contents="Hello!\n Teodors",
+           attachments="design.txt")
