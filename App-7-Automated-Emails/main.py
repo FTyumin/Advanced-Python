984ebe99b359c8bec5d8afebb9db1ccf22abdf5f
FTyumin <feodor.tjumin28@gmail.com>
2022-10-26 12:08:00 +0300

    Code Refactoring

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -4,22 +4,25 @@ from news import NewsFeed
 import datetime
 import time
 
+
+def send_email():
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=yesterday,
+                         to_date=today)
+    email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today",
+               contents=f"Hello row['name']!\n See what's on about {row['interest']} today.{news_feed.get()}\n Teodors")
+
+
 while True:
     if datetime.datetime.now().hour == 11 and datetime.datetime.now().minute == 55:
 
         df = pandas.read_excel('people.xlsx')
 
         for index, row in df.iterrows():
-            today = datetime.datetime.now().strftime('%Y-%m-%d')
-            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-            news_feed = NewsFeed(interest=row['interest'],
-                                 from_date=yesterday,
-                                 to_date=today)
-
-            email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
-            email.send(to=row['email'],
-                   subject=f"Your {row['interest']} news for today",
-                   contents=f"Hello row['name']!\n See what's on about {row['interest']} today.{news_feed.get()}\n Teodors",
-                   attachments="design.txt")
+            send_email()
 
     time.sleep(60)
--- App-7-Automated-Emails/news.py
+++ App-7-Automated-Emails/news.py
@@ -18,22 +18,31 @@ class NewsFeed:
         self.language = language
 
     def get(self):
-        url = f"{self.base_url}" \
-              f"q={self.interest}&" \
-              f"from={self.from_date}&"\
-              f"to={self.to_date}&"\
-              f"language={self.language}" \
-              f"&apiKey={self.api_key}"
+        url = self._build_url()
 
-        response = requests.get(url)
-        content = response.json()
-        articles = content['articles']
+        articles = self._get_articles(url)
 
         email_body = ''
         for article in articles:
             email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
 
         return email_body
+
+    def _get_articles(self, url):
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+        return articles
+
+    def _build_url(self):
+        url = f"{self.base_url}" \
+              f"q={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}" \
+              f"&apiKey={self.api_key}"
+        return url
+
 
 if __name__ == "__main__":
     news_feed = NewsFeed(interest='Biden', from_date='2022-10-18', to_date='2022-10-19', language='en')
