b0e15b05fe084a5574663b95caedfc15532f37cc
FTyumin <feodor.tjumin28@gmail.com>
2022-10-26 11:56:44 +0300

    Time

--- App-7-Automated-Emails/main.py
+++ App-7-Automated-Emails/main.py
@@ -1,15 +1,25 @@
 import yagmail
 import pandas
 from news import NewsFeed
+import datetime
+import time
 
-df = pandas.read_excel('people.xlsx')
+while True:
+    if datetime.datetime.now().hour == 11 and datetime.datetime.now().minute == 55:
 
-for index, row in df.iterrows():
-    news_feed = NewsFeed(interest=row['interest'], from_date='2022-24-10', to_date = '2022-25-10')
+        df = pandas.read_excel('people.xlsx')
 
-    email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
-    email.send(to=row['email'],
-           subject=f"Your {row['interest']} news for today",
-           contents=f"Hello row['name']!\n See what's on about {row['interest']} today.{news_feed.get()}\n Teodors",
-           attachments="design.txt")
+        for index, row in df.iterrows():
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+            news_feed = NewsFeed(interest=row['interest'],
+                                 from_date=yesterday,
+                                 to_date=today)
 
+            email = yagmail.SMTP(user="johnvermont3rd@gmail.com", password="tencpfsvbweaeweu")
+            email.send(to=row['email'],
+                   subject=f"Your {row['interest']} news for today",
+                   contents=f"Hello row['name']!\n See what's on about {row['interest']} today.{news_feed.get()}\n Teodors",
+                   attachments="design.txt")
+
+    time.sleep(60)
