7318c5b675dbbfe13208743f70dfc8fd320648db
FTyumin <feodor.tjumin28@gmail.com>
2022-11-06 23:00:56 +0200

    Make routing automatic

--- App-8-Instant-Dictionary-Webapp/main.py
+++ App-8-Instant-Dictionary-Webapp/main.py
@@ -1,5 +1,6 @@
 import justpy as jp
-
+import inspect
+from webapp import page
 from webapp.about import About
 from webapp.home import Home
 from webapp.dictionary import Dictionary
@@ -7,8 +8,9 @@ from webapp.dictionary import Dictionary
 imports = list(globals().values())
 
 for obj in imports:
-    if hasattr(obj, 'path'):
-        jp.Route(obj.path, obj.serve)
+    if inspect.isclass(obj):
+        if issubclass(obj, page.Page) and obj is not page.Page:
+            jp.Route(obj.path, obj.serve)
 
 
 # jp.Route(Home.path, Home.serve)
