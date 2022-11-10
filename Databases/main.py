fbda7899102158c65998ec8fc42db4a2ceafb040
FTyumin <feodor.tjumin28@gmail.com>
2022-11-10 19:20:22 +0200

    Working with database - part2

--- Databases/main.py
+++ Databases/main.py
@@ -55,7 +55,28 @@ def select_with_condition():
     connection.close()
     return result
 
+def update_value(occupied, seat_id):
+    connection = sqlite3.connect("cinema.db")
+    connection.execute("""
+    UPDATE "Seat" SET "taken"=? WHERE "seat_id"=?
+    """, [occupied,seat_id])
+    connection.commit()
+    connection.close()
+
+def delete_record():
+    connection = sqlite3.connect("cinema.db")
+    connection.execute("""
+    DELETE FROM "Seat" WHERE seat_id = "A3"
+    """)
+    connection.commit()
+    connection.close()
+
+
+
 # insert_record()
 # print(select_all())
 # print(select_specific_columns())
-print(select_with_condition())
+# print(select_with_condition())
+# update_value()
+# delete_record()
+update_value(occupied=0, seat_id="A2")
