2128009c3230cc2985bd300424be80f97f293559
FTyumin <feodor.tjumin28@gmail.com>
2022-11-12 10:34:00 +0200

    Card class

--- App-10-Cinema-Ticket-Booking/main.py
+++ App-10-Cinema-Ticket-Booking/main.py
@@ -70,15 +70,29 @@ class Card():
 
     database = "banking.db"
 
-    def __init__(self,type,number,cvc,holder):
+    def __init__(self, type, number, cvc, holder):
         self.type = type
         self.number = number
         self.cvc = cvc
         self.holder = holder
 
     def validate(self,price):
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT "balance" FROM "Card" WHERE "number"=? and "cvc"=?
+        """, [self.number., self.cvc)
+        result = cursor.fetchall()
 
+        if result:
+            balance = result[0][0]
+            if balance >= price:
+                connection.execute("""
+                UPDATE "Card" SET "balance" = ? WHERE "number"=? and "cvc"=?
+                """, [balance-price, self.number, self.cvc])
+            connection.commit()
+            connection.close()
+        return True
 
 
 class Ticket():
