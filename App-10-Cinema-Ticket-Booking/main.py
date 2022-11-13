3cf9ad2be109a428433d20e8d60bb4d6bc8a3d4a
FTyumin <feodor.tjumin28@gmail.com>
2022-11-13 23:13:05 +0200

    Ticket class

--- App-10-Cinema-Ticket-Booking/main.py
+++ App-10-Cinema-Ticket-Booking/main.py
@@ -101,8 +101,27 @@ class Ticket():
         self.user = user
         self.price = price
         self.id = "".join([random.choice(string.ascii_letters) for i in range(8)])
+        self.seat_number = seat_number
 
     def to_pdf(self):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family="Times", style="B", size = 24)
+        pdf.cell(w=0, h=80, txt="Your Digital Ticker", border=1, ln=1, align="C")
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Name:", border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.user.name, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+        pdf.set_font(family="Times", style="B", size=14)
+        pdf.cell(w=100, h=25, txt="Ticket ID", border=1)
+        pdf.set_font(family='Times', style='', size=12)
+        pdf.cell(w=0, h=25, txt=self.id, border=1, ln=1)
+        pdf.cell(w=0, h=5, txt="", border=0, ln=1)
+
+
 
 
